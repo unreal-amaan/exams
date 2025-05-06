cd923dea8a3c164c67959d8211f82a30ddf3fb4f
UNREAL-AMAAN <amaanuddinsyed2005@gmail.com>
2025-05-06 22:00:22 +0530

    5

--- c#/task9_vscode/Program.cs
+++ c#/task9_vscode/Program.cs
@@ -1,40 +1,25 @@
-using System;
+
 using System.Windows.Forms;
 using System.Drawing;
-
 namespace First
 {
     public class MyForm : Form
     {
-        private TextBox textBox1;
-
         public MyForm()
         {
             InitComponents();
         }
-
         private void InitComponents()
         {
-            // Initialize and add TextBox
-            textBox1 = new TextBox();
-            textBox1.Location = new Point(50, 50);
-            textBox1.Size = new Size(200, 30);
-            Controls.Add(textBox1);
-
-            // Show color dialog
-            ColorDialog dlg = new ColorDialog();
-            if (dlg.ShowDialog() == DialogResult.OK)
-            {
-                // Set the selected color as TextBox background
-                textBox1.BackColor = dlg.Color;
+            ColorDialog dlg = new ColorDialog(); 
+            if(dlg.ShowDialog() == DialogResult.OK) {
+                this.BackColor = dlg.Color;
+                // dlg.ShowDialog();
             }
         }
-
         [STAThread]
-        static void Main()
-        {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
+        static void Main() {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware); Application.EnableVisualStyles();
             Application.Run(new MyForm());
         }
     }
