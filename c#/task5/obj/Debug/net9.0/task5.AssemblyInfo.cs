eee640861bbc72dce62bc6302b1685582bfcc68e
UNREAL-AMAAN <amaanuddinsyed2005@gmail.com>
2025-05-06 21:39:50 +0530

    adhbnj

--- c#/task5/obj/Debug/net9.0/task5.AssemblyInfo.cs
+++ c#/task5/obj/Debug/net9.0/task5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6537396d2c7fa05a99da0603238448d52477784e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9e6230644e7839b659fdd784f8fc8c0964a6679")]
 [assembly: System.Reflection.AssemblyProductAttribute("task5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- c#/task9_vscode/Program.cs
+++ c#/task9_vscode/Program.cs
@@ -1,19 +1,41 @@
-using System.Windows.Forms; using System.Drawing; namespace First
+using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace First
 {
-public class MyForm : Form
-{
-public MyForm()
-{
-InitComponents();
-}
-private void InitComponents()
-{
-ColorDialog dlg = new ColorDialog(); dlg.ShowDialog();
-}
-[STAThread]
-static void Main() {
-Application.SetHighDpiMode(HighDpiMode.SystemAware); Application.EnableVisualStyles();
-Application.Run(new MyForm());
-}
-}
+    public class MyForm : Form
+    {
+        private TextBox textBox1;
+
+        public MyForm()
+        {
+            InitComponents();
+        }
+
+        private void InitComponents()
+        {
+            // Initialize and add TextBox
+            textBox1 = new TextBox();
+            textBox1.Location = new Point(50, 50);
+            textBox1.Size = new Size(200, 30);
+            Controls.Add(textBox1);
+
+            // Show color dialog
+            ColorDialog dlg = new ColorDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                // Set the selected color as TextBox background
+                textBox1.BackColor = dlg.Color;
+            }
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.Run(new MyForm());
+        }
+    }
 }
