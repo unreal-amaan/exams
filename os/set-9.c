f208a3edf1795eb822d872de757aecd7cda4cdbd
syed amaan <160302715+unreal-amaan@users.noreply.github.com>
2025-05-09 10:12:54 +0530

    Update set-9.c

--- os/set-9.c
+++ os/set-9.c
@@ -1,60 +1,43 @@
-#include <stdio.h>
-#define MAX 25
+#include<stdio.h> 
+#define max 25 
+void main() 
+{  int frag[max],b[max],f[max],i,j,nb,nf,temp,highest=0; 
+static int bf[max],ff[max]; 
+printf("Memory management Scheme-first fit"); 
+printf("\nenter number of blocks:"); 
+scanf("%d",&nb); 
+printf("\n enter the number of files:"); 
+scanf("%d",&nf); 
+printf("\n enter size of blocks:"); 
+for(i=1;i<=nb;i++) 
+{     printf("\nblock %d:",i); 
+scanf("%d",&b[i]); 
+} 
+printf("\n enter size of files:"); 
+for(i=1;i<=nf;i++) 
+{ 
+printf("\nfile %d:",i); 
+scanf("%d",&f[i]); 
+} 
+for(i=1;i<=nf;i++) 
+{ 
+for(j=1;j<=nb;j++) 
+{ 
+if(bf[j]!=1) 
+{ 
+temp=b[j]-f[i]; 
+if(temp>=0) 
 
-void main()
-{
-    int frag[MAX], b[MAX], f[MAX], i, j, nb, nf, temp, lowest;
-    static int bf[MAX], ff[MAX];
-
-    printf("\nEnter the number of blocks: ");
-    scanf("%d", &nb);
-    printf("\nEnter the number of files: ");
-    scanf("%d", &nf);
-
-    printf("\nEnter the size of the blocks:\n");
-    for (i = 1; i <= nb; i++)
-    {
-        printf("Block %d: ", i);
-        scanf("%d", &b[i]);
-    }
-
-    printf("\nEnter the size of the files:\n");
-    for (i = 1; i <= nf; i++)
-    {
-        printf("File %d: ", i);
-        scanf("%d", &f[i]);
-    }
-
-    for (i = 1; i <= nf; i++)
-    {
-        lowest = 10000; // reset for each file
-        for (j = 1; j <= nb; j++)
-        {
-            if (bf[j] != 1)
-            { // if block is free
-                temp = b[j] - f[i];
-                if (temp >= 0 && temp < lowest)
-                { // best fit condition
-                    ff[i] = j;
-                    lowest = temp;
-                }
-            }
-        }
-
-        frag[i] = lowest;
-
-        if (lowest != 10000) // if block found
-            bf[ff[i]] = 1;
-        else
-            ff[i] = -1; // no block found
-    }
-
-    printf("\nFile No\tFile Size\tBlock No\tBlock Size\tFragment\n");
-    for (i = 1; i <= nf; i++)
-    {
-        if (ff[i] != -1)
-            printf("%d\t%d\t\t%d\t\t%d\t\t%d\n", i, f[i], ff[i], b[ff[i]], frag[i]);
-        else
-            printf("%d\t%d\t\tNot Allocated\n", i, f[i]);
-    }
+{ 
+ff[i]=j; 
+break;  
+} 
+} 
+} 
+frag[i]=temp; 
+bf[ff[i]]=1; 
+} 
+printf("\n file no\tfile size\tblock no\tblocksize\tfragment"); 
+for(i=1;i<=nf;i++) 
+printf("\n %d  \t %d \t %d \t %d \t %d",i,f[i],ff[i],b[ff[i]],frag[i]); 
 }
